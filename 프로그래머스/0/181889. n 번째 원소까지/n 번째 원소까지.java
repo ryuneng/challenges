14c81b7d03e2d7489b85a96afc68d9a79589f4b4
유리빛나 <ryuneng2@gmail.com>
2024-05-29 15:17:05 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,13 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-    public List solution(int[] num_list, int n) {
-        List<Integer> answer = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            answer.add(num_list[i]);
-        }
-        
-        return answer;
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list, 0, n);
     }
 }
