d1b88e581be3bfab42546874d603eb1540fe03d4
유리빛나 <ryuneng2@gmail.com>
2024-05-30 13:29:36 +0900

    [level 0] Title: 카운트 업, Time: 1.37 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,11 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int startNum, int endNum) {
-        int[] answer = new int[endNum - startNum + 1];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = startNum + i;
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(startNum, endNum).toArray();
     }
 }
